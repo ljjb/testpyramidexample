eddec8bcdbbf980363e161683bbfa97d5e2ec2ff
Leo Bouillet <leojjbouillet@gmail.com>
2021-02-24 17:40:37 +1100

    update: radical comment

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxDemoServerApplication.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxDemoServerApplication.java
@@ -2,6 +2,7 @@ package rxdemo.endpoints;
 
 import spark.servlet.SparkApplication;
 
+// COMMENT
 public class RxDemoServerApplication implements SparkApplication {
     @Override
     public void init() {
