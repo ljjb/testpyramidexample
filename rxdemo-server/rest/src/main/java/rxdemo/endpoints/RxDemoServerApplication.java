b0f7f4aecbc9732246b6f900acfd0e609cbedab2
Leo Bouillet <leojjbouillet@gmail.com>
2021-02-24 17:45:10 +1100

    update: borp blorp

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxDemoServerApplication.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxDemoServerApplication.java
@@ -2,6 +2,7 @@ package rxdemo.endpoints;
 
 import spark.servlet.SparkApplication;
 
+// HUH?
 public class RxDemoServerApplication implements SparkApplication {
     @Override
     public void init() {
